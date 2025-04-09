2acf946d70d8deac521913207b69e821940c89bb
gawelskitools <sascha.gawel@gmx.de>
2025-04-09 17:23:54 +0200

    Create beitragsarten.js

---
+++ beitragsarten.js
@@ -0,0 +1,10 @@
+// --- beitragsarten.js ---
+
+export const beitragsartenMapping = {
+  nachricht: ["#newsclip", "#erklärung", "#kontext"],
+  reportage: ["#reportage", "#portraet", "#thema", "#kontext"],
+  doku: ["#thema", "#investigativ", "#kontext"],
+  interview: ["#interview"],
+  magazinbeitrag: ["#kontext"],
+  portraet: ["#portraet"]
+};
